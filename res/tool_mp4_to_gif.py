95d84805f696fd489bf5f22246ee481865cb8232
zzy <1770484498@qq.com>
2020-06-30 21:53:34 +0800

    update final 2020.6.30

--- res/tool_mp4_to_gif.py
+++ res/tool_mp4_to_gif.py
@@ -1,7 +1,8 @@
 from moviepy.editor import *
 
 v = VideoFileClip("res/demo.mp4")
-VideoFileClip("res/demo.mp4").subclip(0, 10).write_gif("res/1.gif")
-VideoFileClip("res/demo.mp4").subclip(10, 20).write_gif("res/2.gif")
-VideoFileClip("res/demo.mp4").subclip(20, 30).write_gif("res/3.gif")
-VideoFileClip("res/demo.mp4").subclip(147, 157).write_gif("res/4.gif")
+v = v.resize((v.size[0]//2,v.size[1]//2))
+v.subclip(0, 10).write_gif("res/1.gif")
+v.subclip(10, 20).write_gif("res/2.gif")
+v.subclip(20, 30).write_gif("res/3.gif")
+v.subclip(147, 157).write_gif("res/4.gif")
